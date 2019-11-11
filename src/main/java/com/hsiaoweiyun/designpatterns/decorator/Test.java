5ea29cb5e5e3b33ed5b492458a6adf7c74337029
Victor.Hsiao <victor.hsiao@tung-shen.com>
2019-11-11 16:47:34 +0800

    新增一些說明

--- src/main/java/com/hsiaoweiyun/designpatterns/decorator/Test.java
+++ src/main/java/com/hsiaoweiyun/designpatterns/decorator/Test.java
@@ -5,6 +5,7 @@ package com.hsiaoweiyun.designpatterns.decorator;
  */
 public class Test {
 
+    //decorator 模式, 優點: 可以動態的為物件添加功能, 比如java InputStream FileInputStream. 缺點: 會產生很多小物件
     public static void main(String[] args){
         Beverage espresso = new Espresso();
         espresso = new Milk(espresso);   //espresso 加 牛奶
